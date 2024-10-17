d026ff2b8f3a8194b87da4c831813095f39011c2
wcarbajal <wcarbajal@outlook.com>
2024-10-17 18:12:55 -0500

    antes del update

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -2,11 +2,12 @@ import { BadRequestException, Injectable, InternalServerErrorException, Logger, 
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Product } from './entities/product.entity';
+import { Product, ProductImage } from './entities';
 import { Repository } from 'typeorm';
 import { PaginationDto } from '../common/dtos/pagination.dto';
 
 import {  validate as  isUUID } from "uuid";
+
 
 @Injectable()
 export class ProductsService {
@@ -18,6 +19,10 @@ export class ProductsService {
     @InjectRepository( Product )
     private readonly productRepository: Repository<Product>,
 
+    @InjectRepository( ProductImage )
+    private readonly productImagesRepository: Repository<ProductImage>,
+
+
   ) { }
 
 
@@ -25,12 +30,15 @@ export class ProductsService {
 
     try {
 
+      const { images = [], ...productDetails } = createProductDto;
       
       const product = await this.productRepository.create( {
-        ...createProductDto, 
-      images: []     } );
+        ...productDetails, 
+      images: images.map( image =>  this.productImagesRepository.create( {url: image} ))
+     });
+
       await this.productRepository.save( product );
-      return product;
+      return {...product, images};
 
     } catch ( error: any ) {
 
@@ -41,39 +49,57 @@ export class ProductsService {
   }
 
   //todo:paginar
-  findAll( paginationDto: PaginationDto) {
+  async findAll( paginationDto: PaginationDto) {
 
     const { limit = 10, offset = 0 } = paginationDto;
     
-    const products = this.productRepository.find({
+    const products = await this.productRepository.find({
       take: limit,
       skip: offset,
-      //todo:relaciones
+      relations: {
+        images: true,
+      }
     });
 
-    return products;
+    
+    return products.map( product => ({
+      ...product, 
+      images: product.images.map( image => image.url)}));;
   }
 
   async findOne( term: string ) {
     
     let product: Product;
+    
 
     if ( isUUID( term ) ) {
-      product = await this.productRepository.findOneBy( {id: term} );
+      product = await this.productRepository.findOneBy( {id: term});
     } else{
-      const queryBuilder = this.productRepository.createQueryBuilder();
+      const queryBuilder = this.productRepository.createQueryBuilder('prod');
       product = await queryBuilder
                 .where( 'UPPER(slug) =:slug or title =:title', {
                   slug: term.toUpperCase(),
                   title: term.toLowerCase()
                 } )
+                .leftJoinAndSelect('prod.images', 'prodImages')
                 .getOne();    
     }
     
     if ( !product ) {
       throw new NotFoundException( `Product with ${ term } not found` );
     }
+    
     return product;
+  }
+
+
+  async findOnePlain(term: string){
+
+    const  {images, ...rest} = await this.findOne( term );
+    
+
+    return {...rest , images: images.map( image => image.url) };
+
   }
 
   async update( id: string, updateProductDto: UpdateProductDto ) {
