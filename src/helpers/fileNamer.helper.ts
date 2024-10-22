845a0c95bbe2d2c5f9f8eeaa418174a3486a57f9
wcarbajal <wcarbajal@outlook.com>
2024-10-22 18:03:39 -0500

    grabar archivos completo

--- src/helpers/fileNamer.helper.ts
+++ src/helpers/fileNamer.helper.ts
@@ -1,17 +1,22 @@
+import { v4 as uuid } from "uuid";
+
 export const fileNamer = ( req: Express.Request, file: Express.Multer.File, callback: Function ) => {
 
-  
-
-  if( !file) return callback( new Error( 'File is empty' ), false );
-
-  const ext = file.mimetype.split( '/' )[1];
-
-  const fileName = `HolaMundo.${ext}  `;
-  //callback( null, `${Date.now()}-${file.originalname.split( '.' )[0]}.${ext}`);
 
 
+  if ( !file ) return callback( new Error( 'File is empty' ), false );
+  const ext = file.mimetype.split( '/' )[ 1 ];
+  const fecha = new Date().getTimezoneOffset();
+  console.log( fecha );
+  /* const nombre = file.originalname.split( '.' )[0]
 
 
-  callback(null, fileName);
+  const fileName = `${nombre}.${ext}`; */
+  callback( null, `${ uuid() }-${ file.originalname.split( '.' )[ 0 ] }.${ ext }` );
+
+
+
+
+  //callback(null, fileName);
 }
 
